8e532f3398fb34ca253cefc0bd0d08d3740cab75
adamtuenti <adanavarrete15@gmail.com>
2020-03-18 14:51:50 -0500

    cambios

--- Solinal-Front/src/Home.js
+++ Solinal-Front/src/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View, SafeAreaView,FlatList, TouchableHighlight,Image, TextInput,Alert,TouchableOpacity } from 'react-native';
 import { Input, Button, SocialIcon, Icon } from 'react-native-elements';
-
+import { Container, Header, Content, Accordion } from "native-base";
+import * as Font from 'expo-font';
 
 
 
@@ -11,6 +12,12 @@ export default class Home extends Component{
 
     render() {
     return (
+    <Container>
+      <Header
+  leftComponent={{ icon: 'menu', color: '#fff' }}
+  centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+  rightComponent={{ icon: 'home', color: '#fff' }}
+/>
       // Try setting `flexDirection` to `column`.
       <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'green', alignItems: 'center', justifyContent: 'center'}}>
 
@@ -31,6 +38,7 @@ export default class Home extends Component{
             onPress={() => this.props.navigation.navigate('Tab')}
         />
       </View>
+    </Container>    
     );
   }
 
