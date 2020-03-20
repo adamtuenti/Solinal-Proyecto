fbbd810a575ed4b344900b42dc16fe23982f572f
adamtuenti <adanavarrete15@gmail.com>
2020-03-19 22:09:27 -0500

    cambios en los estilos de la app

--- Solinal-Front/src/Main.js
+++ Solinal-Front/src/Main.js
@@ -28,7 +28,7 @@ export default class Main extends Component {
 
 
 
-        <Header style={{backgroundColor: '#1ed695',height:75}}>
+        <Header style={{justifyContent: 'flex-end',marginTop:5,backgroundColor: '#1ed695',height:75, alignItems: 'center',}}>
           <Left>
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%201.png'}}
                             style= {{height: 40,
@@ -51,127 +51,189 @@ export default class Main extends Component {
 
 
         <Content padder style={{backgroundColor: '#f6f6f6'}}>
-          <Card transparent style={{backgroundColor: '#f6f6f6'}}>
-            <CardItem>
-              <Left style={{width:175}}>
+          <View style={{flex: 1, flexDirection: 'row', margin:5,padding:10}}>
+          <View style={{marginRight:35}}>
               <Text style={{color: '#636363'}}>
-                  Auditorias realizadas: # 
+                  Auditorias realizadas: #
                 
                 <Text style={{color: '#2ba855'}}>
                   0
                 </Text>
                 </Text>
+          </View>
 
-                </Left>
-                <Body />
-              <Right>
+          <View>
+               
                 <Text style={{color: '#2ba855'}}>
                   CUENTA GRATUITA
                 </Text>
-              </Right>
-            </CardItem>
-          </Card>
+          </View>
+            
+          </View>
+
+
           <Card>
+
             <CardItem bordered>
-              <Left>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+
+              <View style={{marginRight:10}}>
+                
+
+
+
+
+          
               <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%206.png'}}
                             style= {{height: 30,
                                      width: 40}}>
               </Image>
-              </Left>
-              <Body>
+              </View>
+
+              <View style={{marginRight:10}}>
+            
+           
                 <Text style={{color: '#636363'}}>Crear Equipo</Text>
-              </Body>
-              <Right />
+              </View>
+              
+              </View>
+            
             </CardItem>
+
+
             <CardItem bordered>
-              <Left>
-              <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%207.png'}}
-                            style= {{height: 30,
-                                     width: 40}}>
-              </Image>
-              </Left>
-              <Body>
-                <Text style={{color: '#636363'}}>
-                  Mis Auditorías
-                </Text>
-              </Body>
-              <Right />
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{marginRight:25}}>      
+                  <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%207.png'}}
+                                style= {{height: 35,
+                                        width: 25}}>
+                  </Image>
+                  </View>
+                <View>  
+                  <Text style={{color: '#636363'}}>
+                    Mis Auditorías
+                  </Text>
+                </View>
+              
+              </View>
+
             </CardItem>
+
+
+
+
             <CardItem bordered>
-              <Left>
+
+
+              <View style={{flex: 1, flexDirection: 'row'}}>
+
+              <View style={{marginRight:15}}>
+          
               <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%208.png'}}
                             style= {{height: 30,
-                                     width: 40}}>
+                                     width: 35}}>
+              
               </Image>
-              </Left>
-              <Body>
+              </View>
+              <View>
+            
+           
                 <Text style={{color: '#636363'}}>Recomienda Solinal Auditor</Text>
-              </Body>
-              <Right />
+              </View>
+              
+              </View>
+             
+
+
+
             </CardItem>
           </Card>
+
+
           <Card>
-            <CardItem>
+
+
+
+
+        <View style={{flex: 1, flexDirection: 'row', height:100, justifyContent:'flex-end', marginTop:15}}>
+
+          <View style={{margin:7}}>
+            
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%209.png'}}
-                            style= {{height: 60,
-                                     width: 80}}>
+                            style= {{height: 50,
+                                     width: 70}}>
               </Image>
-              <Body>
+
+          </View>
+
+
+        <View style={{flex: 1, flexDirection: 'column',marginLeft:5}}>
+              
+
+
                 <Text style={{color: '#1ed695'}}>CONVIÉRTETE EN PREMIUM</Text>
                 <Text style={{color: '#636363'}}>Vea estadísticas de cumplimiento, cierre no conformidades, o crea más checklists de auditoría</Text>
-              </Body>
-              <Right>
+              
+
+        </View>
+
+        <View style={{marginLeft:5, marginRight:3}}>    
               <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%2010.png'}}
-                            style= {{height: 53,
-                                     width: 30}}>
+                            style= {{height: 50,
+                                     width: 24}}>
               </Image>
-              </Right>
-             </CardItem>
-           </Card>
+        
+        </View>
+
+       </View>
+
+      </Card>
+        
+       
+              
         </Content>
 
         
-        <Footer>
+        <Footer style={{height:63}}>
           <FooterTab style={{backgroundColor: '#f6f6f6'}}>
           
               
             <Button onPress={()=>this.props.navigation.navigate('AuditoriasVacia')}>
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%2011.png'}}
-                            style= {{height: 25,
-                                     width: 25}}>
+                            style= {{height: 29,
+                                     width: 23}}>
               </Image>
-              <Text style={{color: '#636363',fontSize:10}}>Auditorias</Text>
+              <Text style={{color: '#636363',fontSize:9}}>Auditorias</Text>
             </Button>
             
           
             <Button vertical>
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%2012.png'}}
-                            style= {{height: 25,
-                                     width: 25}}>
+                            style= {{height: 29,
+                                     width: 27}}>
               </Image>
-              <Text style={{color: '#636363',fontSize:10}}>Calendario</Text>
+              <Text style={{color: '#636363',fontSize:9}}>Calendario</Text>
             </Button>
 
             <Button vertical>                  
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%2013.png'}}
-                            style= {{height: 25,
-                                     width: 25}}>
+                            style= {{height: 45,
+                                     width: 45}}>
               </Image>
               </Button>
             <Button vertical>
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%2014.png'}}
-                            style= {{height: 25,
+                            style= {{height: 29,
                                      width: 25}}>
               </Image>
-              <Text style={{color: '#636363',fontSize:10}}>Acción Correctiva</Text>
+              <Text style={{color: '#636363',fontSize:9}}>Acción Correctiva</Text>
             </Button>
             <Button vertical>
             <Image source={{uri: 'https://raw.githubusercontent.com/adamtuenti/Solinal-Proyecto/master/Solinal-Front/png/Recurso%2015.png'}}
-                            style= {{height: 25,
+                            style= {{height: 29,
                                      width: 25}}>
               </Image>
-              <Text style={{color: '#636363', fontSize:10}}>No Conformidad</Text>
+              <Text style={{color: '#636363', fontSize:9}}>No Conformidad</Text>
             </Button>
             
           </FooterTab>
