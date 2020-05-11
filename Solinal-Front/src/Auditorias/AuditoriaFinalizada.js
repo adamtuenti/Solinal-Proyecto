3e721572977d2431a4035e4a86e1d16713a8ab54
Jonathan Vivas <jvivas.trecool@gmail.com>
2020-05-10 20:51:44 -0500

    Update AuditoriaFinalizada.js

--- Solinal-Front/src/Auditorias/AuditoriaFinalizada.js
+++ Solinal-Front/src/Auditorias/AuditoriaFinalizada.js
@@ -22,16 +22,101 @@ import RNHTMLtoPDF from 'react-native-html-to-pdf';
 import PDFLib, { PDFDocument, PDFPage } from 'react-native-pdf-lib';
 import RNPrint from 'react-native-print';
 
+const html = '';
+
 
 export default class AuditoriaFinalizada extends Component{
 
-  state = {
-    selectedPrinter: null,
-  };
+  constructor(props) {
+    super(props);
+    this.state = { 
+      loading: false,
+      users: [],
+      url: 'http://accountsolinal.pythonanywhere.com/api/users',
+      };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      /*Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")*/
+    });
+    this.setState({ isReady: true });
+  }
+
+  componentDidMount = () => {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+    const array = [];
+    console.log(this.state.url)
+      this.setState({loading:true})
+      fetch(this.state.url)
+
+      .then(res=>res.json())
+     
+      .then(res=>{ 
+        console.log('--')
+          console.log(res);
+          this.setState({
+          users: res,
+          url: res.next,
+          loading: false,    
+          })
+          console.log('----')
+          console.log(this.state.users);
+      })
+      
+  }
+
+  renderTableData() {
+    const array = this.state.users
+
+    array.forEach(function(element){
+      var iduser = element.user
+      var nombre = element.first_name
+      var apellido = element.last_name
+      var correo = element.email
+      var tipoC = element.tipoCuenta
+
+      return (
+        <tr key={iduser}>
+           <td>{iduser}</td>
+           <td>{nombre}</td>
+           <td>{apellido}</td>
+           <td>{email}</td>
+           <td>{tipoC}</td>
+        </tr>
+     )
+    });
+ }
+
+ renderTableHeader() {
+  let header = Object.keys(this.state.users[0])
+  return header.map((key, index) => {
+     return <th key={index}>{key.toUpperCase()}</th>
+  })
+}
+
+  makeHTML = () => {
+    const htmlstring = `
+    <div>
+      <h1 id='title'>Tabla de Usuarios</h1>
+        <table id='usuario'>
+               <tbody>
+               `+this.renderTableData()+`
+               </tbody>
+            </table>
+    </div>
+ `
+ return htmlstring;
+  }
 
   expoPDF = async () => {
     let filePath = await Print.printToFileAsync({
-      html: "<h1>PDF TEST SOLINAL</h1>",
+      html: this.makeHTML(),
       width: 612,
       height: 792,
       base64: false
@@ -51,101 +136,6 @@ export default class AuditoriaFinalizada extends Component{
        });
     });
   }
-
-    createPDF = async () => {
-        const page1 = PDFPage
-.create()
-.setMediaBox(200, 200)
-.drawText('You can add text and rectangles to the PDF!', {
-  x: 5,
-  y: 235,
-  color: '#007386',
-})
-.drawRectangle({
-  x: 25,
-  y: 25,
-  width: 150,
-  height: 150,
-  color: '#FF99CC',
-})
-.drawRectangle({
-  x: 75,
-  y: 75,
-  width: 50,
-  height: 50,
-  color: '#99FFCC',
-});
-
-const page2 = PDFPage
-  .create()
-  .setMediaBox(250, 250)
-  .drawText('You can add JPG images too!')
-  
-
-  const docsDir =  await PDFLib.getDocumentsDirectory();
-const pdfPath = `${docsDir}/sample.pdf`;
-PDFDocument
-  .create(pdfPath)
-  .addPages(page1, page2)
-  .write() // Returns a promise that resolves with the PDF's path
-  .then(path => {
-    console.log('PDF created at: ' + path);
-    // Do stuff with your shiny new PDF!
-  });
-    }
-
-    constructor(props) {
-        super(props)
-        this.state ={}
-    };
-
-    generatePDF = () => {
-        var document = new jsPDF('p', 'pt');
-        
-        document.text(20, 20, 'This is the first title.')
-  
-        document.setFont('helvetica')
-        document.setFontType('normal')
-        document.text(20, 60, 'This is the second title.')
-  
-        document.setFont('helvetica')
-        document.setFontType('normal')
-        document.text(20, 100, 'This is the thrid title.')      
-  
-        
-        document.save('demo.pdf')
-      }   
-    
-    async componentDidMount() {
-        await Font.loadAsync({
-          Roboto: require("native-base/Fonts/Roboto.ttf"),
-          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-          /*Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")*/
-        });
-        this.setState({ isReady: true });
-      }
-
-      async printHTML() {
-        await RNPrint.print({
-          html:
-            '<h1>Here will be Heading 1</h1><h2>Here will be Heading 2</h2><h3>Here will be Heading 3</h3>',
-        });
-      }
-
-      async printPDF() {
-        const results = await RNHTMLtoPDF.convert({
-          html: '<h1>Demo Text to converted to PDF</h1>',
-          fileName: 'test',
-          base64: true,
-        });
-        await RNPrint.print({ filePath: results.filePath });
-      }
-
-      async printRemotePDF() {
-        await RNPrint.print({
-          filePath: 'http://www.africau.edu/images/default/sample.pdf',
-        });
-      }
 
       render() {
         return (
